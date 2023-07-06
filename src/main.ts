0f2721e0a0dae022c63e030d9e8ef2b52d466f9a
bolta <freshness.and.speed.2007@gmail.com>
2023-07-06 14:11:53 +0900

    前処理の要否判定や処理の呼び出し等を本体側で行うようにした
    シェルスクリプトは Windows だと動かしにくいので依存しないようにする

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,19 @@ const writeFile = promisify(fs.writeFile);
 const exec = promisify(exec_);
 import { renderToNewCanvas } from './renderer';
 import { Spec } from './spec';
+import { preprocess } from './preprocess';
 
 (async () => {
 	// 絶対パスにしないと解決されない場合あり
-	const path = fs.realpathSync(process.argv[2]);
-	const spec: Spec = (await import(path)).spec;
-
-	const canvas = renderToNewCanvas(spec.size, spec.divider, spec.colors);
-
-	const outPath = path.replace(/\.[^.]*$/, '.png');
+	const srcPath = fs.realpathSync(process.argv[2]);
+	const inPath = preprocess(srcPath) ?? srcPath;
+	const spec: Spec = (await import(inPath)).spec;
+	const outPath = inPath.replace(/\.[^.]*$/, '.png');
 	if (fs.existsSync(outPath)) {
 		console.log(`${outPath} already exists.`);
 	} else {
+		const canvas = renderToNewCanvas(spec.size, spec.divider, spec.colors);
 		await writeFile(outPath, canvas.toBuffer());
 	}
 	await exec(`cmd.exe /c start ${outPath}`);
 })();
-
--- src/preprocess.ts
+++ src/preprocess.ts
@@ -1,17 +1,42 @@
 import * as fs from 'fs';
-import { Seq } from 'lazy-sequences'
+import Seq from 'lazy-sequences';
 import * as uuid from 'uuid';
+import * as path from 'path';
+import moment from 'moment';
 
-const rawSrc = process.argv[2];
-const tilesSrcDir = process.argv[3];
-if (! rawSrc || ! tilesSrcDir) throw 'specify a file to preprocess and a path to tiles src/ dir';
+const tilesSrcDir = path.resolve(path.dirname(process.argv[1]));
 
-const srcLines = new Seq(fs.readFileSync(rawSrc, 'utf-8').split('\n'));
+// Windows でも / を使う
+const relativePathWithSlashes = (from, to) => path.relative(from, to).replace(/\\/g, '/');
 
-const destLines = srcLines.map(line => line.trim().startsWith('import')
-		? line.replace(/(['"])\.\.\/src/, `$1${tilesSrcDir}`)
-		: line.replace(/\$seed/, `{ seed: '${uuid.v4()}' }`));
+// このコメントがソースファイルの先頭にあるときのみ処理済と見なす
+const PREPROCESSED_MARKER = '// PREPROCESSED';
 
-for (const line of destLines) {
-	console.log(line);
-}
+/**
+ * srcPath のファイルが前処理されていない場合、前処理して処理後のパスを返す。
+ * すでに前処理されている場合は null を返す
+ */
+export const preprocess = (srcPath) => {
+	const srcContent = fs.readFileSync(srcPath, 'utf-8');
+	if (srcContent.includes(PREPROCESSED_MARKER)) return null;
+
+	const srcLines = new Seq(srcContent.split('\n'));
+	const { name: srcName, ext: srcExt } = path.parse(srcPath);
+	const destDir = path.join(tilesSrcDir, '..', 'atelier', srcName);
+
+	const destLines =
+			new Seq([PREPROCESSED_MARKER])
+			.concat(srcLines.map(line => line.trim().startsWith('import')
+					? line.replace(/(['"])\.\.\/src\//, `$1${relativePathWithSlashes(destDir, tilesSrcDir)}/`)
+					: line.replace(/\$seed/, `{ seed: '${uuid.v4()}' }`)));
+	const destContent = destLines.map(line => `${line}\n`).collect().join('');
+
+	const timestampRun = moment().format('YYYYMMDD_HHmmss');
+	const destPath = path.join(destDir, `${srcName}.${timestampRun}${srcExt}`);
+
+	fs.mkdirSync(destDir, { recursive: true });
+	fs.writeFileSync(destPath, destContent, { flag: 'wx' });
+
+	return destPath;
+};
+
