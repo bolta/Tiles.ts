6f61e6cf9c508fc168d9c048da1dcc8be085fe87
bolta <freshness.and.speed.2007@gmail.com>
2023-07-06 11:49:32 +0900

    依存ライブラリとメソッドシグネチャの変更に追従

--- resources/template.ts
+++ resources/template.ts
@@ -1,4 +1,3 @@
-import polygonsIntersect from 'polygons-intersect';
 import { xy } from '../src/polygon';
 import { RandomWalkColorGenerator } from '../src/color/color_generator';
 import { lrtb } from '../src/divider/matrix';
@@ -10,8 +9,8 @@ export const spec: Spec = {
 	size: xy(800, 600),
 
 	divider: composite([
-		lrtb(xy(200, 200)),
-		lrtb(xy(  8,   8)),
+		lrtb({ tileSize: xy(200, 200) }),
+		lrtb({ tileSize: xy(  8,   8) }),
 	]),
 
 	colors: new RandomWalkColorGenerator({ ...$seed }),
